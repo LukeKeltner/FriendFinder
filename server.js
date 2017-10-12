29fd8a0d6b9f776adf1d2083fa885cac134f2731
Luke Keltner <LKelt@hawken.edu>
2017-10-12 12:06:40 -0400

    routers are up!

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+var htmlRoutes = require(path.join(__dirname, "app", "routing", "htmlRoutes.js"))
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -11,16 +12,18 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 app.use(express.static(path.join(__dirname, "app", "public")));
 
-
-app.get("/", function(req, res)
+app.use("", htmlRoutes)
+//htmlRoutes(app)
+/*app.get("/", function(req, res)
 {
 	res.sendFile(path.join(__dirname, "app", "public", "home.html"))
 })
 
-/*app.get("/survey", function(req, res)
+app.get("/survey", function(req, res)
 {
 	res.sendFile(path.join(__dirname, "app", "public", "survey.html"))
 })*/
+
 /*app.get("/homeScript.js", function(req, res)
 {
 	res.sendFile(path.join(__dirname, "app", "public", "homeScript.js"))
